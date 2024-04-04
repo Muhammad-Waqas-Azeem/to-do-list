edec0f6d0d6ef29b424ba46e4fbf3c8284aa2379
Muhammad Waqas Azeem <160093061+Muhammad-Waqas-Azeem@users.noreply.github.com>
2024-04-04 19:29:04 +0500

    basic modfs

---
+++ index.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+let todolist = [];
+do {
+    var addtodo = await inquirer.prompt([
+        {
+            name: "additem",
+            message: "Enter the item to add",
+            type: "input",
+        },
+        {
+            name: "moreitem",
+            message: "Do you want to add more items",
+            type: "confirm",
+            default: false,
+        },
+    ]);
+    todolist.push(addtodo.additem);
+    console.log(todolist);
+} while (addtodo.moreitem);
